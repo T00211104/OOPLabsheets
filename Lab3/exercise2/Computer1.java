2fe92e6e29d881d6ea7575d1bd8d854927de544e
Jordan <jordanmcyt01@gmail.com>
2020-10-02 13:52:14 +0100

    First Commit of the OOPLabsheets Project

---
+++ Lab3/exercise2/Computer1.java
@@ -0,0 +1,75 @@
+package Lab3.exercise2;
+
+public class Computer1 {
+    private String manufacturer;
+    private String type;
+    private double speed;
+    private int RAM;
+    private double price;
+
+
+    public Computer1() {
+        manufacturer = "Not available";
+        type = "Not available";
+        speed = 0;
+        RAM = 0;
+        price = 0;
+    }
+    public Computer1(String manufacturer,String type,double speed,int RAM,double price) {
+        this.manufacturer = manufacturer;
+        this.type = type;
+        this.speed = speed;
+        this.RAM = RAM;
+        this.price = price;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public int getRAM() {
+        return RAM;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setSpeed(double speed) {
+        if(speed>0)
+            this.speed = speed;
+    }
+
+    public void setRAM(int RAM) {
+        RAM = -8;
+        this.RAM = RAM;
+    }
+
+    public void setPrice(double price) {
+        price=875;
+        this.price = price;
+    }
+
+    public String toString() {
+        return  "Manufacturer: " + getManufacturer() + "   Type: " + getType() + "   Speed: " + getSpeed()
+                + "   RAM: " + getRAM() + "   Price: " + getPrice();
+    }
+}
+
+
---
+++ Lab3/exercise2/TestComputer1.java
@@ -0,0 +1,32 @@
+package Lab3.exercise2;
+
+import javax.swing.*;
+
+public class TestComputer1 {
+    public static void main(String[] args) {
+        String output = "";
+
+        Computer1 c3 = new Computer1();
+
+        output += "Calling the no-argument Computer constructor. " +
+                "The first Computer object details are: \n\n" + c3.toString();
+
+        Computer1 c5 = new Computer1();
+
+        output += "\n\nCalling the no-argument Computer constructor. " +
+                "The first Computer object details are: \n\n" + c5.toString();
+
+        Computer1 c4 = new Computer1("Apple", "Desktop", 2.9, 12, 865.00);
+
+        c3.setSpeed(-3.15);
+
+        output += "\n\nCalling the multi-argument Computer constructor. " +
+                "The second Computer object details are: \n\n" + c4.toString();
+
+        JOptionPane.showMessageDialog(null,output,"Computer Object Data",
+                JOptionPane.INFORMATION_MESSAGE);
+
+        System.exit(0);
+    }
+}
+
